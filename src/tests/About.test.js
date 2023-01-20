38b1399fb4ac7ddd3a4757f0299bc13a3c73f0d8
Mateus Kulik <mateuskulik30@gmail.com>
2023-01-20 15:43:42 -0300

    Requisito 2 testa se a página contém informações sobre a pokédex

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import { React } from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './helper/RenderWithRounter';
+import { About } from '../pages';
+
+describe(' Teste o componente <About.js />.', () => {
+  test('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+    const aboutPokedex = screen.queryByRole('heading', { name: 'About Pokédex' });
+    expect(aboutPokedex).toBeInTheDocument();
+    const text = screen
+      .getByText(/this application simulates a pokédex, a digital encyclopedia containing all pokémon/i);
+    expect(text).toBeInTheDocument();
+    const moreText = screen
+      .getByText(/one can filter pokémon by type, and see more details for each one of them/i);
+    expect(moreText).toBeInTheDocument();
+    const img = screen.getByRole('img', { name: /pokédex/i });
+    expect(img.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
