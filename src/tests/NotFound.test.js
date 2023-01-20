c4338d4796aa8e760270958d58b142b46175a896
Mateus Kulik <mateuskulik30@gmail.com>
2023-01-20 16:22:44 -0300

    Requisito 4 - testa se existe as informações dentro de  notFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import { React } from 'react';
+import { screen, act } from '@testing-library/react';
+import renderWithRouter from './helper/RenderWithRounter';
+import App from '../App';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/pagina-que-nao-existe/');
+    });
+    const getErrorText = screen.getByRole('heading', { level: 2, name: /page requested not found/i });
+    expect(getErrorText).toBeInTheDocument();
+  });
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push('/pagina-que-nao-existe/');
+    });
+    const img = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
